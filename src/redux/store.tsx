774b7f3ee080b35aa8a824b0eacd5c5e7f68a244
Alejo <alejoivanrojas@gmail.com>
2023-01-16 11:02:08 -0300

    types added

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -5,3 +5,9 @@ import tweetReducer from "./tweetSlice";
 const store = configureStore({
     reducer: tweetReducer
 })
+
+//Types for typescript
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch 
+
+export default store
